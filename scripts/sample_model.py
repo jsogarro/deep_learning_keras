265d9863a2befab5c42758b6e289e7cff1623ee9
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-02 00:31:56 -0500

    add main

--- scripts/sample_model.py
+++ scripts/sample_model.py
@@ -8,14 +8,17 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
 
+def main():
+    X, y = make_circles(n_samples=1000,
+                        noise=0.,
+                        factor=0.2,
+                        random_state=0)
 
-X, y = make_circles(n_samples=1000,
-                    noise=0.,
-                    factor=0.2,
-                    random_state=0)
+    model = Sequential()
+    model.add(Dense(4, input_shape=(2,), activation='tanh'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(SGD(lr=0.5), 'binary_crossentropy', metrics=['accuracy'])
+    model.fit(X, y, epochs=20)
 
-model = Sequential()
-model.add(Dense(4, input_shape=(2,), activation='tanh'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(SGD(lr=0.5), 'binary_crossentropy', metrics=['accuracy'])
-model.fit(X, y, epochs=20)
+if __name__ == '__main__':
+    main()
--- scripts/sound_wave.py
+++ scripts/sound_wave.py
@@ -4,18 +4,21 @@ Sound Wave - sample file that shows how to load and plot wav files
 from scipy.io import wavfile
 import matplotlib.pyplot as plt 
 
+def main():
+    # load our wav file
+    rate, snd = wavfile.read(filename='../data/sms.wav')
 
-# load our wav file
-rate, snd = wavfile.read(filename='../data/sms.wav')
+    # plot the wav 
+    plt.plot(snd)
+    plt.title('SMS Sound Wav')
+    plt.show()
 
-# plot the wav 
-plt.plot(snd)
-plt.title('SMS Sound Wav')
-plt.show()
+    # show spectragram
+    _ = plt.specgram(snd, NFFT=1024, Fs=44100
+    plt.title('Wav Spectragam')
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('Time (s)')
+    plt.show()
 
-# show spectragram
-_ = plt.specgram(snd, NFFT=1024, Fs=44100
-plt.title('Wav Spectragam')
-plt.ylabel('Frequency (Hz)')
-plt.xlabel('Time (s)')
-plt.show()
+if __name__ == '__main__':
+    main()
