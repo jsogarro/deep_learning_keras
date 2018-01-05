6f058a97955b6f8bf6a9a77cef1fa2a988d6d301
Jamal O'Garro <jamal.ogarro[at]gmail.com>
2018-01-05 05:11:01 -0500

    add cross validation and average score

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -1,13 +1,11 @@
 """
-Cross Validation - example of using validating our model using several
-k-fold cross validation techniques and optimizing it using paralleization
-techniques
+Cross Validation - example of using validating our model k-fold cross validation
 """
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
 
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.models import Sequential
@@ -26,8 +24,19 @@ def build_logistic_regression_model():
 
 
 def main():
-    build_logistic_regression_model()
+    # import our data
+    df = pd.read_csv('../data/user_visit_duration.csv')
 
+    # build model, inputs and outputs
+    X = df[['Time (min)']].values
+    y = df['Buy'].values
+
+    # train and cross validate our model using k-fold validation with 3 partitions
+    model = KerasClassifier(build_fn=build_logistic_regression_model, epochs=50)
+    cv = KFold(3, shuffle=True)
+    scores = cross_val_score(model, X, y, cv=cv)
+    print('Cross Validation Scores: {}'.format(scores))
+    print('Average Score: {}'.format(np.average(scores)))
 
 if __name__ == '__main__':
     main()
